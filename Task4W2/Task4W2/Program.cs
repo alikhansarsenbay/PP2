dfe8e68436e208c490f5caaddbfc4a6ffcb6fab4
alikhansarsenbay <sarsenbai_alihan@mail.ru>
2019-02-15 02:28:52 +0600

    Lol

---
+++ Task4W2/Task4W2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4W2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath1 = @"C:\Users\alecb\Desktop\PP2\week2\path\file.txt";//указывает путь
+            string FilePath2 = @"C:\Users\alecb\Desktop\PP2\week2\path1\file.txt";//указывает еще один путь
+            StreamWriter sw = new StreamWriter(FilePath1);//создаём файл
+            sw.WriteLine("random string");
+            sw.Close();
+            File.Copy(FilePath1, FilePath2);//копирует с первого во второй 
+            File.Delete(FilePath1);//удаляет первый
+
+        }
+    }
+}
