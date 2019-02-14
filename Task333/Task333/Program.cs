2edb4dc5475a261e295c578883604f34ce22f4ee
alikhansarsenbay <sarsenbai_alihan@mail.ru>
2019-02-14 22:56:49 +0600

    GGG

---
+++ Task333/Task333/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task333
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            string elements = Console.ReadLine();
+            string[] array = elements.Split();//используем сплит чтоб поделить массив на ячейки
+
+            int n = int.Parse(num);//переводим строку в число
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+                Console.Write(array[i]);//повторяем числа
+                Console.Write(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
+
